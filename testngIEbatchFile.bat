47b3c62c3e101f0b34ffad7f72fbe961b32438cf
ritica <rramakrishnan@errorfree.com>
2017-09-08 17:22:47 -0700

    testng batch file for ie test for login

---
+++ testngIEbatchFile.bat
@@ -0,0 +1,5 @@
+et projectLocation=C:\Users\rramakrishnan\workspace\MultibrowserTest
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+java org.testng.TestNG %projectLocation%\testng.xml
+pause
