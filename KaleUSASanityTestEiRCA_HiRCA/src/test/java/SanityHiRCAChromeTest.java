c66d05d1b569dff236ce2dbb86cf3e19321b0164
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-27 04:43:11 +0000

    SanityHiRCAChromeTest.java edited online with Bitbucket

--- KaleUSASanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAChromeTest.java
+++ KaleUSASanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAChromeTest.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.openqa.selenium.NoSuchElementException;
 
 public class SanityHiRCAChromeTest {
 	private WebDriver driver;
@@ -21,6 +21,7 @@ public class SanityHiRCAChromeTest {
 	private String chrome_path = "C:\\Users\\rramakrishnan\\DriversForSelenium\\chromedriver.exe";
 	private String url = "https://kale.error-free.com/";
 	private String EventTitleChrome = "Sanity Test Chrome";	
+	private int login =0;
   
 	@Before
 	  public void beforeTest() throws MalformedURLException{
@@ -50,6 +51,21 @@ public class SanityHiRCAChromeTest {
 		  driver.findElement(By.id("pii-pw")).sendKeys(password);
 		  //Sign in button is located and clicked
 		  driver.findElement(By.id("pii-signin-button")).click();
+		  WebElement element = driver.findElement(By.id("pii-signin-message"));
+                String text = element.getText();
+                if (element.isDisplayed())
+                {
+                       if(text.isEmpty())
+                             System.out.println("Logged in");
+                       else
+                       {
+                             driver.findElement(By.id("pii-pw")).sendKeys(password);
+                             //Sign in button is located and clicked
+                             driver.findElement(By.id("pii-signin-button")).click();
+                             login =1;
+                       }
+                                            
+                }
 	  }
 	  
 	  @Test
@@ -61,7 +77,15 @@ public class SanityHiRCAChromeTest {
 		  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		  
 		  driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
-		  
+		  try{
+                       if (login==1)
+                       {
+                             WebDriverWait wait2 = new WebDriverWait(driver,20);
+                             wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-close"))).click();
+                       }
+                }catch (NoSuchElementException e){
+                       throw e;
+                }
 		  //Clicks on Analysis
 		  try
 		  {
