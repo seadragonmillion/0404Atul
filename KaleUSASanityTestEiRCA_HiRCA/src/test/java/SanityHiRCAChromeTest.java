2ef5ab865c2df2f70a6e04ce1587cbd471d2c23f
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-27 20:32:26 +0000

    SanityHiRCAChromeTest.java edited online with Bitbucket

--- KaleUSASanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAChromeTest.java
+++ KaleUSASanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAChromeTest.java
@@ -68,6 +68,29 @@ public class SanityHiRCAChromeTest {
                 }
 	  }
 	  
+	  public void deleteNewRecord(String recordName) throws Exception{
+			  
+			  //CLicks on first newly created record
+			  driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-irca']/ul/li[2]/a")).click();
+			  //Clicks on delete button
+			  driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-single']/div/div/a[3]")).click();
+			  WebDriverWait wait = new WebDriverWait(driver,10);
+			  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-title")));
+			  //Clicks on delete report
+			  driver.findElement(By.id("pii-user-home-dialog-confirmed")).click();
+			  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-note")));
+			  Thread.sleep(2000);
+			  driver.findElement(By.id("pii-user-home-panel-btn-irca")).click();
+			  //Verify record deleted
+			  //Click on 1st record
+			  String name = driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-irca']/ul/li[2]/a")).getText();
+			  System.out.println(name);
+			  if (name!=recordName)
+				  System.out.println("Record deleted");
+			  else
+				  System.out.println("Record could not be deleted");
+			  			  
+		  }
 
 	  
 	  @Test
