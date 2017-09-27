d4bda8c9280b466ab8107e1ad18b55e8ac5d3c9e
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-27 18:20:47 +0000

    SanityHiRCAInternetExplorerTest.java edited online with Bitbucket

--- KaleAsiaSanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAInternetExplorerTest.java
+++ KaleAsiaSanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAInternetExplorerTest.java
@@ -76,6 +76,31 @@ public class SanityHiRCAInternetExplorerTest {
                 }
 	  }
 	  
+	  public void deleteNewRecord(String recordName) throws Exception{
+		  
+		  JavascriptExecutor jse = (JavascriptExecutor)driver;
+		  //CLicks on first newly created record
+		  driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-irca']/ul/li[2]/a")).click();
+		  //Clicks on delete button
+		  driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-single']/div/div/a[3]")).click();
+		  WebDriverWait wait = new WebDriverWait(driver,10);
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-title")));
+		  //Clicks on delete report
+		  jse.executeScript("return document.getElementById('pii-user-home-dialog-confirmed').click();");
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-note")));
+		  Thread.sleep(2000);
+		  jse.executeScript("return document.getElementById('pii-user-home-panel-btn-irca').click();");
+		  //Verify record deleted
+		  //Click on 1st record
+		  String name = driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-irca']/ul/li[2]/a")).getText();
+		  System.out.println(name);
+		  if (name!=recordName)
+			  System.out.println("Record deleted");
+		  else
+			  System.out.println("Record could not be deleted");
+		  			  
+	  }
+	  
 	  @Test
 	  public void SanityTest() throws Exception{
 		  
@@ -135,6 +160,8 @@ public class SanityHiRCAInternetExplorerTest {
 		  else
 			  System.out.println ("Record not found.");
 		  assertEquals(name, recordName);
+		  //Deletes the newly created record
+		  deleteNewRecord(recordName);
 		  //Clicks on logout
 		  jse.executeScript("return document.getElementById('pii-user-loginname').click();");
 		  jse.executeScript("return document.getElementById('pii-signout-button').click();");
