0185604b8f1fef3158b0d62ed8b8ef8722353118
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-27 20:33:21 +0000

    SanityHiRCAFirefoxTest.java edited online with Bitbucket

--- KaleUSASanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAFirefoxTest.java
+++ KaleUSASanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAFirefoxTest.java
@@ -65,6 +65,30 @@ public class SanityHiRCAFirefoxTest {
                 }
 		  }
 		  
+		  public void deleteNewRecord(String recordName) throws Exception{
+			  
+			  //CLicks on first newly created record
+			  driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-irca']/ul/li[2]/a")).click();
+			  //Clicks on delete button
+			  driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-single']/div/div/a[3]")).click();
+			  WebDriverWait wait = new WebDriverWait(driver,10);
+			  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-title")));
+			  //Clicks on delete report
+			  driver.findElement(By.id("pii-user-home-dialog-confirmed")).click();
+			  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-note")));
+			  Thread.sleep(2000);
+			  driver.findElement(By.id("pii-user-home-panel-btn-irca")).click();
+			  //Verify record deleted
+			  //Click on 1st record
+			  String name = driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-irca']/ul/li[2]/a")).getText();
+			  System.out.println(name);
+			  if (name!=recordName)
+				  System.out.println("Record deleted");
+			  else
+				  System.out.println("Record could not be deleted");
+			  			  
+		  }
+		  
 		  @Test
 		  public void SanityTest() throws Exception{
 			  
@@ -114,6 +138,8 @@ public class SanityHiRCAFirefoxTest {
 			  else
 				  System.out.println ("Record not found.");
 			  assertEquals(name, recordName);
+			  //Deletes the newly created record
+			  deleteNewRecord(recordName);
 			  driver.findElement(By.id("pii-user-loginname")).click();
 			  driver.findElement(By.id("pii-signout-button")).click();
 			  afterTest();
