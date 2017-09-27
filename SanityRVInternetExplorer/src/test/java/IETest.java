166053c07bb1ea18b3a146f3035eff7d4594a8ec
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-27 18:00:16 +0000

    IETest.java edited online with Bitbucket

--- SanityRVInternetExplorer/src/test/java/IETest.java
+++ SanityRVInternetExplorer/src/test/java/IETest.java
@@ -34,7 +34,7 @@ public class IETest {
 		  System.setProperty("webdriver.ie.driver",ie_path);
 		  DesiredCapabilities cap = new DesiredCapabilities(); 
 		  cap.setCapability("ignoreZoomSettings", true);
-		  cap.setCapability("requireWindowFocus", true);
+		 // cap.setCapability("requireWindowFocus", true);
 		  driver = new InternetExplorerDriver(cap);
 		  //Browser is maximized
 		  driver.manage().window().maximize();
