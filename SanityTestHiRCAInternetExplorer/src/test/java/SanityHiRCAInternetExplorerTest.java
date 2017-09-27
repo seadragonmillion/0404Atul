d544da01faf9d8f61f42c19291b94d2dde323068
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-27 17:57:41 +0000

    SanityHiRCAInternetExplorerTest.java edited online with Bitbucket

--- SanityTestHiRCAInternetExplorer/src/test/java/SanityHiRCAInternetExplorerTest.java
+++ SanityTestHiRCAInternetExplorer/src/test/java/SanityHiRCAInternetExplorerTest.java
@@ -26,7 +26,7 @@ public class SanityHiRCAInternetExplorerTest {
 		  System.setProperty("webdriver.ie.driver","C:\\Users\\rramakrishnan\\DriversForSelenium\\IEDriverServer.exe");
 		  DesiredCapabilities cap = new DesiredCapabilities(); 
 		  cap.setCapability("ignoreZoomSettings", true);
-		  cap.setCapability("requireWindowFocus", true);
+		  //cap.setCapability("requireWindowFocus", true);
 		  driver = new InternetExplorerDriver(cap);
 		  //Browser is maximized
 		  driver.manage().window().maximize();
