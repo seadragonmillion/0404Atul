a6741378ee7ecb45204871680a59e35b2560bd97
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-28 16:40:05 +0000

    SanityHiRCAChromeTest.java edited online with Bitbucket

--- KaleUSASanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAChromeTest.java
+++ KaleUSASanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAChromeTest.java
@@ -37,7 +37,7 @@ public class SanityHiRCAChromeTest {
 	  }
 	  
 	
-	  public void Login() {
+	  public void Login() throws Exception{
 		  
 		  System.out.println("Title before login: "+driver.getTitle());
 		  //Login button is located and clicked
