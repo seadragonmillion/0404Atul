024ae5221cd089f4b13031b80af10990da310df7
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-28 19:37:14 +0000

    SanityHiRCAChromeTest.java edited online with Bitbucket

--- SanityTestHiRCA/src/test/java/SanityHiRCAChromeTest.java
+++ SanityTestHiRCA/src/test/java/SanityHiRCAChromeTest.java
@@ -79,32 +79,36 @@ public class SanityHiRCAChromeTest {
 			  }
 			
 		  }
-		  else
-		  {
-			  while(c>0)
-			  {
-				  Thread.sleep(1000);
-				  driver.findElement(By.id("pii-un")).clear();
-				  driver.findElement(By.id("pii-pw")).clear();
-				  //Username text field is located and the username is entered
-				  driver.findElement(By.id("pii-un")).sendKeys(username);
-				  //Password field is located and the password is entered
-				  driver.findElement(By.id("pii-pw")).sendKeys(password);
-				  c=c+1;
-				  if (user.equals(username)==true)
+		  if ((user.equals(username)==false)||(pw.equals(password)==false))
+		    {
+				  while(c>0)
 				  {
-					  if(pw.equals(password)==true)
+					  Thread.sleep(1000);
+					  driver.findElement(By.id("pii-un")).clear();
+					  driver.findElement(By.id("pii-pw")).clear();
+					  //Username text field is located and the username is entered
+					  driver.findElement(By.id("pii-un")).sendKeys(username);
+					  //Password field is located and the password is entered
+					  driver.findElement(By.id("pii-pw")).sendKeys(password);
+					  user = driver.findElement(By.id("pii-un")).getAttribute("value");
+					  pw = driver.findElement(By.id("pii-pw")).getAttribute("value");
+					  if (user.equals(username)==true)
 					  {
-						  //Sign in button is located and clicked
-						  driver.findElement(By.id("pii-signin-button")).click();
-						  break;
+						  if(pw.equals(password)==true)
+						  {
+							  //Sign in button is located and clicked
+							  driver.findElement(By.id("pii-signin-button")).click();
+							  break;
+						  }
+						
 					  }
-					
 				  }
-			  }
+			  
+			  
+		  }
 		  }
 			  
-		      }
+		      
 		  
 		    public void deleteNewRecord(String recordName) throws Exception{
 			  
@@ -134,6 +138,7 @@ public class SanityHiRCAChromeTest {
 		  public void SanityTest() throws Exception{
 			  
 			  Login();
+			  Thread.sleep(5000);
 			  System.out.println("Title after login: "+driver.getTitle());
 			 
 			 // assertEquals (driver.getTitle(), "KALE�");
