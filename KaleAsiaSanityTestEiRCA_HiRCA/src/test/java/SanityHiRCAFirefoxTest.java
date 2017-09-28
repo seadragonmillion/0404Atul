d899d9d3b02bd204a5f47284242be9bfc1edb352
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-28 16:22:13 +0000

    SanityHiRCAFirefoxTest.java edited online with Bitbucket

--- KaleAsiaSanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAFirefoxTest.java
+++ KaleAsiaSanityTestEiRCA_HiRCA/src/test/java/SanityHiRCAFirefoxTest.java
@@ -34,7 +34,7 @@ public class SanityHiRCAFirefoxTest {
 		  }
 		  
 		
-		  public void Login() {
+		  public void Login() throws Exception{
 			  
 			  System.out.println("Title before login: "+driver.getTitle());
 			  //Login button is located and clicked
