fce262d081a59317597fd491268b70ba387069ae
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-29 03:40:17 +0000

    SanityTestEiRCA_IETest.java edited online with Bitbucket

--- KaleAsiaSanityTestEiRCA_HiRCA/src/test/java/SanityTestEiRCA_IETest.java
+++ KaleAsiaSanityTestEiRCA_HiRCA/src/test/java/SanityTestEiRCA_IETest.java
@@ -64,23 +64,30 @@ public class SanityTestEiRCA_IETest {
 			  if(pw.equals(password)==true)
 			  {
 				  //Sign in button is located and clicked
-				  jse.executeScript("return document.getElementById('pii-signin-button').click();");  
+				  jse.executeScript("return document.getElementById('pii-signin-button').click();"); 
+				   while(c>0)
+				  {
+				  Thread.sleep(2000);
 				  WebElement element = driver.findElement(By.id("pii-signin-message"));
 				  String text = element.getText();
 				  if (element.isDisplayed())
 				  {
 					  if(text.isEmpty())
+					  {
 						  System.out.println("Logged in");
+						  break;
+						  }
 					  else
 					  {
 						  driver.findElement(By.id("pii-pw")).sendKeys(password);
 						  //Sign in button is located and clicked
 						  jse.executeScript("return document.getElementById('pii-signin-button').click();");
 						  login =1;
+						  break;
 					  }
 					  			  
 				  }
-			  }
+			  }}
 			
 		  }
 		  if ((user.equals(username)==false)||(pw.equals(password)==false))
