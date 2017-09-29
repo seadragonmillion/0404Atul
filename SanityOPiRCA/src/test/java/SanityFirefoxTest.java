230128aef7f7e461b6c78859b6cdca02e3f5f4e0
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-29 00:17:01 +0000

    SanityFirefoxTest.java edited online with Bitbucket

--- SanityOPiRCA/src/test/java/SanityFirefoxTest.java
+++ SanityOPiRCA/src/test/java/SanityFirefoxTest.java
@@ -67,22 +67,29 @@ public class SanityFirefoxTest {
 			  {
 				  //Sign in button is located and clicked
 				  driver.findElement(By.id("pii-signin-button")).click();  
+				  while(c>0)
+				  {
+				  Thread.sleep(2000);
 				  WebElement element = driver.findElement(By.id("pii-signin-message"));
 				  String text = element.getText();
 				  if (element.isDisplayed())
 				  {
 					  if(text.isEmpty())
+					  {
 						  System.out.println("Logged in");
+						  break;
+						  }
 					  else
 					  {
 						  driver.findElement(By.id("pii-pw")).sendKeys(password);
 						  //Sign in button is located and clicked
 						  driver.findElement(By.id("pii-signin-button")).click();
 						  login =1;
+						  break;
 					  }
 					  			  
 				  }
-			  }
+			  }}
 			
 		  }
 		  if ((user.equals(username)==false)||(pw.equals(password)==false))
