3e917d39dcb67e15e4d02f20557577c59caa6db7
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-09-29 00:10:29 +0000

    SanityTestRV_ChromeTest.java edited online with Bitbucket

--- SanityTestRemoteVerification/src/test/java/SanityTestRV_ChromeTest.java
+++ SanityTestRemoteVerification/src/test/java/SanityTestRV_ChromeTest.java
@@ -66,6 +66,7 @@ public class SanityTestRV_ChromeTest {
 				  driver.findElement(By.id("pii-signin-button")).click();  
 				  while(c>0)
 				  {
+				  Thread.sleep(2000);
 				  WebElement element = driver.findElement(By.id("pii-signin-message"));
 				  String text = element.getText();
 				  if (element.isDisplayed())
