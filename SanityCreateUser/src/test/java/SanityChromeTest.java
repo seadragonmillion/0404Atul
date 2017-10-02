e630c3b84d664caf03f61c79bcc5bcd88bca22e6
ritica <rramakrishnan@errorfree.com>
2017-10-02 16:43:01 -0700

    create user firefox chrome ie10

---
+++ SanityCreateUser/src/test/java/SanityChromeTest.java
@@ -0,0 +1,364 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class SanityChromeTest {
+
+	private WebDriver driver;
+	private String username ="ritica";
+	private String password = "Kale24982417@";
+	private String company_id="sanitytest";
+	private String chrome_path = "C:\\Users\\rramakrishnan\\DriversForSelenium\\chromedriver.exe";
+	private String url = "https://kaledev.error-free.com/";
+	private int login =0;
+		  
+	@Before
+	  public void beforeTest() throws MalformedURLException{
+		  
+		  System.out.println("Performing sanity test on create Company/Group/User in Chrome");
+		  System.setProperty("webdriver.chrome.driver",chrome_path);
+		  driver = new ChromeDriver();
+		  //Browser is maximized
+		  driver.manage().window().maximize();
+		  //Browser navigates to the KALE url
+		  driver.navigate().to(url);
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  }
+	  
+
+	public void Login() throws Exception{
+		  
+		  System.out.println("Title before login: "+driver.getTitle());
+		  //Login button is located and clicked
+		  driver.findElement(By.id("pii-login-button")).click();
+		  //Login pop up is located and clicked
+		  WebDriverWait wait = new WebDriverWait(driver,10);
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("popupLogin"))).click();
+		  //Username text field is located and the username is entered
+		  driver.findElement(By.id("pii-un")).sendKeys(username);
+		  //Password field is located and the password is entered
+		  driver.findElement(By.id("pii-pw")).sendKeys(password);
+		  //Sign in button is located and clicked
+		  String user = driver.findElement(By.id("pii-un")).getAttribute("value");
+		  String pw = driver.findElement(By.id("pii-pw")).getAttribute("value");
+		  int c=1;
+		  if (user.equals(username)==true)
+		  {
+			  if(pw.equals(password)==true)
+			  {
+				  //Sign in button is located and clicked
+				  driver.findElement(By.id("pii-signin-button")).click();  
+				  while(c>0)
+				  {
+				  Thread.sleep(2000);
+				  WebElement element = driver.findElement(By.id("pii-signin-message"));
+				  String text = element.getText();
+				  if (element.isDisplayed())
+				  {
+					  if(text.isEmpty())
+					  {
+						  System.out.println("Logged in");
+						  break;
+						  }
+					  else
+					  {
+						  driver.findElement(By.id("pii-pw")).sendKeys(password);
+						  //Sign in button is located and clicked
+						  driver.findElement(By.id("pii-signin-button")).click();
+						  login =1;
+						  break;
+					  }
+					  			  
+				  }
+				  else break;
+			  }}
+			
+		  }
+		  if ((user.equals(username)==false)||(pw.equals(password)==false))
+		    {
+				  while(c>0)
+				  {
+					  Thread.sleep(1000);
+					  driver.findElement(By.id("pii-un")).clear();
+					  driver.findElement(By.id("pii-pw")).clear();
+					  //Username text field is located and the username is entered
+					  driver.findElement(By.id("pii-un")).sendKeys(username);
+					  //Password field is located and the password is entered
+					  driver.findElement(By.id("pii-pw")).sendKeys(password);
+					  user = driver.findElement(By.id("pii-un")).getAttribute("value");
+					  pw = driver.findElement(By.id("pii-pw")).getAttribute("value");
+					  if (user.equals(username)==true)
+					  {
+						  if(pw.equals(password)==true)
+						  {
+							  //Sign in button is located and clicked
+							  driver.findElement(By.id("pii-signin-button")).click();
+							  break;
+						  }
+						
+					  }
+				  }
+			  
+			  
+		  }
+			  
+		}
+	
+	@Test
+	  public void SanityTest() throws Exception{
+		  
+		Login();
+		  System.out.println("Title after login: "+driver.getTitle());
+		  //Waits for the page to load
+	      driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		  //Switches to the iframe
+		  driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
+		  try{
+                   if (login==1)
+                   {
+                         WebDriverWait wait2 = new WebDriverWait(driver,20);
+                         wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-close"))).click();
+                   }
+            }catch (NoSuchElementException e){
+                   throw e;
+            }
+		  Thread.sleep(5000);
+		  WebDriverWait wait = new WebDriverWait(driver,20);
+		  //Clicks on admin user name on top right corner
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-loginname"))).click();
+		  //Clicks on admin option
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-admin"))).click();
+		  //Clicks on Accounts
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-accounts']/h3/a")).click();
+		  //Clicks on Companies
+		  driver.findElement(By.id("pii-admin-customers-button")).click();
+		  //CLicks on new button
+		  driver.findElement(By.id("pii-admin-cust-button-new")).click();
+		  Thread.sleep(2000);
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-cust-dialog-title"))).click();
+		  //Clicks on new company
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-cust-dialog-confirmed"))).click();
+		  //Fills all mandatory details
+		  driver.findElement(By.id("pii-admin-cust-cid")).sendKeys(company_id);
+		  driver.findElement(By.id("pii-admin-cust-name")).sendKeys("Sanity Test");
+		  driver.findElement(By.id("pii-admin-cust-address")).sendKeys("Sanity Test");
+		  driver.findElement(By.id("pii-admin-cust-tel")).sendKeys("123456");
+		  driver.findElement(By.id("pii-admin-cust-busCts")).sendKeys("Sanity Test");
+		  driver.findElement(By.id("pii-admin-cust-tecCts")).sendKeys("Sanity Test"); 
+		  String ev1 = driver.findElement(By.id("pii-admin-cust-cid")).getAttribute("value");
+		  String ev2 = driver.findElement(By.id("pii-admin-cust-name")).getAttribute("value");
+		  String ev3 = driver.findElement(By.id("pii-admin-cust-address")).getAttribute("value");
+		  String ev4 = driver.findElement(By.id("pii-admin-cust-tel")).getAttribute("value");
+		  String ev5 = driver.findElement(By.id("pii-admin-cust-busCts")).getAttribute("value");
+		  String ev6 = driver.findElement(By.id("pii-admin-cust-tecCts")).getAttribute("value");
+		  if ((ev1.equals(company_id)==false))
+		  {
+			  driver.findElement(By.id("pii-admin-cust-cid")).clear();
+			  driver.findElement(By.id("pii-admin-cust-cid")).sendKeys(company_id);
+		  }
+		  if ((ev2.equals("Sanity Test")==false))
+		  {
+			  driver.findElement(By.id("pii-admin-cust-name")).clear();
+			  driver.findElement(By.id("pii-admin-cust-name")).sendKeys("Sanity Test");
+		  }
+		  if ((ev3.equals("Sanity Test")==false))
+		  {
+			  driver.findElement(By.id("pii-admin-cust-address")).clear();
+			  driver.findElement(By.id("pii-admin-cust-address")).sendKeys("Sanity Test");
+		  }
+		  if ((ev4.equals("123456")==false))
+		  {
+			  driver.findElement(By.id("pii-admin-cust-tel")).clear();
+			  driver.findElement(By.id("pii-admin-cust-tel")).sendKeys("123456");
+		  }
+		  if ((ev5.equals("Sanity Test")==false))
+		  {
+			  driver.findElement(By.id("pii-admin-cust-busCts")).clear();
+			  driver.findElement(By.id("pii-admin-cust-busCts")).sendKeys("Sanity Test");
+		  }
+		  if ((ev6.equals("Sanity Test")==false))
+		  {
+			  driver.findElement(By.id("pii-admin-cust-tecCts")).clear();
+			  driver.findElement(By.id("pii-admin-cust-tecCts")).sendKeys("Sanity Test");
+		  }
+		  //Clicks on save
+		  driver.findElement(By.id("pii-admin-cust-button-save")).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-cust-dialog-title"))).click();
+		  //Clicks on create company
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-cust-dialog-confirmed"))).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-note")));
+		  //Enters company id in ID field 
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-cust-jsgrid']/div/table/tbody/tr[2]/td/input")).sendKeys(company_id);
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-cust-jsgrid']/div/table/tbody/tr[2]/td/input")).sendKeys(Keys.ENTER);
+		  //Clicks on newly created company id
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-cust-jsgrid']/div[2]/table/tbody/tr/td")).click();
+		  System.out.println("Company created");
+		  
+		  
+		  Thread.sleep(2000);
+		  //Clicks on create group
+		  driver.findElement(By.id("pii-admin-group-create")).click();
+		  Thread.sleep(2000);
+		  //Fills all mandatory 
+		  driver.findElement(By.id("pii-admin-group-name")).sendKeys(company_id);
+		  driver.findElement(By.id("pii-admin-group-cases")).sendKeys("all");
+		  ev1 = driver.findElement(By.id("pii-admin-group-name")).getAttribute("value");
+		  ev2 = driver.findElement(By.id("pii-admin-group-cases")).getAttribute("value");
+		  if ((ev1.equals(company_id)==false))
+		  {
+			  driver.findElement(By.id("pii-admin-group-name")).clear();
+			  driver.findElement(By.id("pii-admin-group-name")).sendKeys(company_id);
+		  }
+		  if ((ev2.equals("all")==false))
+		  {
+			  driver.findElement(By.id("pii-admin-group-cases")).clear();
+			  driver.findElement(By.id("pii-admin-group-cases")).sendKeys("all");
+		  }
+		  WebElement element = driver.findElement(By.id("pii-admin-group-cid"));
+		  Select dropdown = new Select (element);
+		  dropdown.selectByVisibleText(company_id);
+		  driver.findElement(By.id("pii-admin-group-modules-button")).click();
+		  WebElement ele = driver.findElement(By.id("pii-admin-group-modules-listbox-popup"));
+		  ele.findElement(By.linkText("Equipment Failure Instant RCA")).click();
+		  ele.findElement(By.linkText("Instant Common Cause Analysis")).click();
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-group-modules-listbox']/div/a")).click();
+		  //Clicks on save
+		  driver.findElement(By.id("pii-admin-group-button-save")).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-group-dialog-title"))).click();
+		  //Clicks on Save
+		  driver.findElement(By.id("pii-admin-group-dialog-confirmed")).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-note")));
+		  System.out.println("Group created");
+		  
+		  
+		  Thread.sleep(2000);
+		  //Clicks on create user
+		  driver.findElement(By.id("pii-admin-user-create")).click();
+		  //Enters mandatory details
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-user-loginname"))).sendKeys(company_id);
+		  driver.findElement(By.id("pii-admin-user-name")).sendKeys(company_id);
+		  driver.findElement(By.id("pii-admin-user-company")).sendKeys("Sanity Test");
+		  driver.findElement(By.id("pii-admin-user-email")).sendKeys("rramakrishnan@errorfree.com");
+		  ev1 = driver.findElement(By.id("pii-admin-user-name")).getAttribute("value");
+		  ev2 = driver.findElement(By.id("pii-admin-user-company")).getAttribute("value");
+		  ev3 = driver.findElement(By.id("pii-admin-user-email")).getAttribute("value");
+		  if ((ev1.equals(company_id)==false))
+		  {
+			  driver.findElement(By.id("pii-admin-user-name")).clear();
+			  driver.findElement(By.id("pii-admin-user-name")).sendKeys(company_id);
+		  }
+		  if ((ev2.equals("Sanity Test")==false))
+		  {
+			  driver.findElement(By.id("pii-admin-user-company")).clear();
+			  driver.findElement(By.id("pii-admin-user-company")).sendKeys("Sanity Test");
+		  }
+		  if ((ev3.equals("rramakrishnan@errorfree.com")==false))
+		  {
+			  driver.findElement(By.id("pii-admin-user-email")).clear();
+			  driver.findElement(By.id("pii-admin-user-email")).sendKeys("rramakrishnan@errorfree.com");
+		  }
+		  driver.findElement(By.id("pii-admin-user-groups-button")).click();
+		  WebElement ele1 = driver.findElement(By.id("pii-admin-user-groups-menu"));
+		  ele1.findElement(By.linkText(company_id)).click();
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-user-groups-dialog']/div/div/a")).click();
+		  Select dd2 = new Select (driver.findElement(By.id("pii-admin-user-dept")));
+		  dd2.selectByVisibleText("Accounting");
+		  Select dd3 = new Select (driver.findElement(By.id("pii-admin-user-jobtitle")));
+		  dd3.selectByVisibleText("Engineer");
+		  Select dd4 = new Select (driver.findElement(By.id("pii-admin-user-customerId")));
+		  dd4.selectByVisibleText(company_id);
+		  //Clicks on save button
+		  driver.findElement(By.id("pii-admin-user-button-save")).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-user-dialog-title"))).click();
+		  //Clicks on Save button
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-user-dialog-confirmed"))).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-note")));
+		  System.out.println("User created");
+		  
+		  
+		  Thread.sleep(2000);
+		  //Clicks on Edit user
+		  driver.findElement(By.id("pii-admin-user-edit")).click();
+		  Thread.sleep(2000);
+		  //Searches for newly created user
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-admin-user-list']/form/div/input"))).sendKeys(company_id);
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-user-list']/form/div/input")).sendKeys(Keys.ENTER);
+		  //Selects the newly created user
+		  WebElement select = driver.findElement(By.id("pii-admin-user-list"));
+		  WebElement option = select.findElement(By.cssSelector(".ui-li-static.ui-body-inherit.ui-first-child.ui-last-child"));
+		  option.click();
+		  //Clicks on delete
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-user-button-delete"))).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-user-dialog-title"))).click();
+		  //Clicks on delete user
+		  driver.findElement(By.id("pii-admin-user-dialog-confirmed")).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-note")));
+		  System.out.println("User deleted");
+		  
+		  
+		  Thread.sleep(2000);
+		  //Clicks on Edit group
+		  driver.findElement(By.id("pii-admin-group-edit")).click();
+		  Thread.sleep(2000);
+		  //Searches for newly created group
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-admin-group-list']/form/div/input"))).sendKeys(company_id);
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-group-list']/form/div/input")).sendKeys(Keys.ENTER);
+		  //Selects the newly created group
+		  select = driver.findElement(By.id("pii-admin-group-list"));
+		  option = select.findElement(By.cssSelector(".ui-li-static.ui-body-inherit.ui-first-child.ui-last-child"));
+		  option.click();
+		  //Clicks on delete
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-group-button-delete"))).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-group-dialog-title"))).click();
+		  //Clicks on delete group
+		  driver.findElement(By.id("pii-admin-group-dialog-confirmed")).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-note")));
+		  System.out.println("Group deleted");
+		  
+		  
+		  Thread.sleep(2000);
+		  //Clicks on Companies
+		  driver.findElement(By.id("pii-admin-customers-button")).click();
+		  Thread.sleep(2000);
+		  //Enters company id in ID field 
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-admin-cust-jsgrid']/div/table/tbody/tr[2]/td/input"))).clear();
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-cust-jsgrid']/div/table/tbody/tr[2]/td/input")).sendKeys(company_id);
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-cust-jsgrid']/div/table/tbody/tr[2]/td/input")).sendKeys(Keys.ENTER);
+		  //Clicks on newly created company id
+		  driver.findElement(By.xpath(".//*[@id='pii-admin-cust-jsgrid']/div[2]/table/tbody/tr/td")).click();
+		  Thread.sleep(2000);
+		  //Clicks on delete
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-cust-button-delete"))).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-admin-cust-dialog-title"))).click();
+		  //Clicks on delete group
+		  driver.findElement(By.id("pii-admin-cust-dialog-confirmed")).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-note")));
+		  System.out.println("Company deleted");
+		  		  
+		  //Logs out
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-loginname"))).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-signout-button"))).click();
+		  afterTest();
+		  		  
+	}
+	
+	public void afterTest(){
+		
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-login-button")));
+		//Browser closes
+		driver.quit();
+	}
+}
