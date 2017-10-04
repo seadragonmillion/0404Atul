9392c2a9ab92a6b98e2e25909d09a164e6a5d1e1
ritica <rramakrishnan@errorfree.com>
2017-10-04 16:21:49 -0700

    dev equipment pii chrome firefox ie10

---
+++ SanityEquipmentCaseSearchPII/src/test/java/FirefoxTest.java
@@ -0,0 +1,238 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class FirefoxTest {
+
+	private FirefoxDriver driver;
+	private String username ="jenkins";
+	private String password = "Kalejenkins@123";
+	private String gecko_path = "C:\\Users\\rramakrishnan\\DriversForSelenium\\geckodriver.exe";
+	private String url = "https://kaledev.error-free.com/";
+	private int login =0;
+	
+	@Before
+	  public void beforeTest() throws MalformedURLException{
+		  
+		 System.out.println("Performing sanity test on Equipment Performance Search PII in Firefox");
+		 System.setProperty("webdriver.gecko.driver",gecko_path);
+		 ProfilesIni ffProfiles = new ProfilesIni();
+		 FirefoxProfile profile = ffProfiles.getProfile("customFirefox");
+		 DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		 capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+		 driver = new FirefoxDriver(capabilities);
+		  //Browser is maximized
+		  driver.manage().window().maximize();
+		  //Browser navigates to the KALE url
+		  driver.navigate().to(url);
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  }
+
+	public void Login() throws Exception{
+		  
+		  System.out.println("Title before login: "+driver.getTitle());
+		  //Login button is located and clicked
+		  driver.findElement(By.id("pii-login-button")).click();
+		  //Login pop up is located and clicked
+		  WebDriverWait wait = new WebDriverWait(driver,10);
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("popupLogin"))).click();
+		  //Username text field is located and the username is entered
+		  driver.findElement(By.id("pii-un")).sendKeys(username);
+		  //Password field is located and the password is entered
+		  driver.findElement(By.id("pii-pw")).sendKeys(password);
+		  //Sign in button is located and clicked
+		  String user = driver.findElement(By.id("pii-un")).getAttribute("value");
+		  String pw = driver.findElement(By.id("pii-pw")).getAttribute("value");
+		  int c=1;
+		  if (user.equals(username)==true)
+		  {
+			  if(pw.equals(password)==true)
+			  {
+				  //Sign in button is located and clicked
+				  driver.findElement(By.id("pii-signin-button")).click();  
+				  while(c>0)
+				  {
+				  Thread.sleep(2000);
+				  WebElement element = driver.findElement(By.id("pii-signin-message"));
+				  String text = element.getText();
+				  if (element.isDisplayed())
+				  {
+					  if(text.isEmpty())
+					  {
+						  System.out.println("Logged in");
+						  break;
+						  }
+					  else
+					  {
+						  driver.findElement(By.id("pii-pw")).sendKeys(password);
+						  //Sign in button is located and clicked
+						  driver.findElement(By.id("pii-signin-button")).click();
+						  login =1;
+						  break;
+					  }
+					  			  
+				  }
+				  else break;
+			  }}
+			
+		  }
+		  if ((user.equals(username)==false)||(pw.equals(password)==false))
+		    {
+				  while(c>0)
+				  {
+					  Thread.sleep(1000);
+					  driver.findElement(By.id("pii-un")).clear();
+					  driver.findElement(By.id("pii-pw")).clear();
+					  //Username text field is located and the username is entered
+					  driver.findElement(By.id("pii-un")).sendKeys(username);
+					  //Password field is located and the password is entered
+					  driver.findElement(By.id("pii-pw")).sendKeys(password);
+					  user = driver.findElement(By.id("pii-un")).getAttribute("value");
+					  pw = driver.findElement(By.id("pii-pw")).getAttribute("value");
+					  if (user.equals(username)==true)
+					  {
+						  if(pw.equals(password)==true)
+						  {
+							  //Sign in button is located and clicked
+							  driver.findElement(By.id("pii-signin-button")).click();
+							  break;
+						  }
+						
+					  }
+				  }
+			  
+			  
+		  }
+			  
+		}
+	
+	@Test
+	  public void SanityTest() throws Exception{
+		  
+		Login();
+		  System.out.println("Title after login: "+driver.getTitle());
+		  //Waits for the page to load
+	      driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		  //Switches to the iframe
+		  driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
+		  try{
+               if (login==1)
+               {
+                     WebDriverWait wait2 = new WebDriverWait(driver,20);
+                     wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-close"))).click();
+               }
+        }catch (NoSuchElementException e){
+               throw e;
+        }
+		  Thread.sleep(4000);
+		  WebDriverWait wait = new WebDriverWait(driver,20);
+		  //Clicks on Error free bank
+		  try
+		  {
+			  driver.findElement(By.id("pii-main-menu-button-e")).click();
+		  }catch (UnhandledAlertException f){			  
+			  driver.switchTo().alert().dismiss();
+		  }
+		  //Clicks on Equipment performance search (pii)
+		  driver.findElement(By.linkText("Equipment Performance Search (PII)")).click();
+		  //Enters the term
+		  driver.findElement(By.id("pii-efse-searchbykw-input")).sendKeys("test data");
+		  driver.findElement(By.id("pii-efse-searchbykw-input")).sendKeys(Keys.ENTER);
+		  //Clicks on F649
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-collapsible-equip-F1459"))).click();
+		  //Clicks on Show Slides
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-slideshow-button-equip-F1459")));
+		  Thread.sleep(2000);
+		  WebElement element =  driver.findElement(By.id("pii-slideshow-button-equip-F1459"));
+		  String slide = element.getText();
+		  element.sendKeys(Keys.TAB);
+		  element.sendKeys(Keys.ENTER);
+		  driver.findElement(By.id("pii-slideshow-equip-F1459-popup"));
+		  System.out.println(slide);
+		  char c = slide.charAt(12);
+		  char c1 = slide.charAt(13);
+		  int n = (Character.getNumericValue(c)*10)+Character.getNumericValue(c1);
+		  Thread.sleep(1000);
+		  System.out.println("Found Slide 1");
+		  Thread.sleep(500);
+		  driver.findElement(By.linkText("Next")).click();
+		  //Checks if there are slides present
+		  for (int i=2;i<=n;i++)
+		  {
+			  String id = "pii-slideimg-F1459-"+(i-1);
+			  Thread.sleep(1000);
+			  if (driver.findElement(By.id(id)).isDisplayed())
+				  System.out.println("Found Slide "+(i));
+			  else System.out.println("Didn't find slide "+i);
+			  Thread.sleep(500);
+			  driver.findElement(By.linkText("Next")).click();
+		  }
+		  //Clicks on close button
+		  driver.findElement(By.xpath(".//*[@id='pii-slideshow-equip-F1459']/a")).click();
+		  //Clicks on clear
+		  driver.findElement(By.id("pii-efse-clear")).click();
+		  //Enters case id
+		  driver.findElement(By.id("pii-efse-searchbyid-input")).sendKeys("1459");
+		  driver.findElement(By.id("pii-efse-searchbyid-input")).sendKeys(Keys.ENTER);
+		  //Clicks on F649
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-collapsible-equip-F1459"))).click();
+		  //Clicks on Show Slides
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-slideshow-button-equip-F1459")));
+		  Thread.sleep(2000);
+		  element =  driver.findElement(By.id("pii-slideshow-button-equip-F1459"));
+		  slide = element.getText();
+		  element.sendKeys(Keys.TAB);
+		  element.sendKeys(Keys.ENTER);
+		  driver.findElement(By.id("pii-slideshow-equip-F1459-popup"));
+		  System.out.println(slide);
+		  c = slide.charAt(12);
+		  c1 = slide.charAt(13);
+		  n = (Character.getNumericValue(c)*10)+Character.getNumericValue(c1);
+		  Thread.sleep(1000);
+		  System.out.println("Found Slide 1");
+		  Thread.sleep(500);
+		  driver.findElement(By.linkText("Next")).click();
+		  //Checks if there are slides present
+		  for (int i=2;i<=n;i++)
+		  {
+			  String id = "pii-slideimg-F1459-"+(i-1);
+			  Thread.sleep(1000);
+			  if (driver.findElement(By.id(id)).isDisplayed())
+				  System.out.println("Found Slide "+(i));
+			  else System.out.println("Didn't find slide "+i);
+			  Thread.sleep(500);
+			  driver.findElement(By.linkText("Next")).click();
+		  }
+		  //Clicks on close button
+		  driver.findElement(By.xpath(".//*[@id='pii-slideshow-equip-F1459']/a")).click();
+		  //Clicks on clear
+		  driver.findElement(By.id("pii-efse-clear")).click();
+		  //Logs out
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-loginname"))).click();
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-signout-button"))).click();
+		  Thread.sleep(2000);
+		  afterTest();
+		  		  
+	}
+	
+	public void afterTest(){
+		
+		//Browser closes
+		driver.quit();
+	}
+
+}
