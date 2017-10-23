2e3f437472652aa8f8514d26dd3afc69f3c22863
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-10-23 23:14:38 +0000

    IETest.java edited online with Bitbucket

--- KaleAsiaSanityElectricalCaseSearch/src/test/java/IETest.java
+++ KaleAsiaSanityElectricalCaseSearch/src/test/java/IETest.java
@@ -32,7 +32,7 @@ public class IETest {
 	
 	@SuppressWarnings("deprecation")
 	@Rule
-	  public Timeout globalTimeout= new Timeout(240000);
+	  public Timeout globalTimeout= new Timeout(600000);
 	@Before
 	  public void beforeTest() throws MalformedURLException{
 		  
