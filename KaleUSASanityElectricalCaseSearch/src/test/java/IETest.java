b6afcbb22a883e772b3a2b85d07632f5508351b0
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-10-23 23:22:02 +0000

    IETest.java edited online with Bitbucket

--- KaleUSASanityElectricalCaseSearch/src/test/java/IETest.java
+++ KaleUSASanityElectricalCaseSearch/src/test/java/IETest.java
@@ -31,7 +31,7 @@ public class IETest {
 
 	@SuppressWarnings("deprecation")
 	@Rule
-	  public Timeout globalTimeout= new Timeout(240000);
+	  public Timeout globalTimeout= new Timeout(600000);
 	
 	
 	@Before
