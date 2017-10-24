3a3e41683afce2da1bde8f5eab5a31e6e254d0af
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-10-24 18:19:25 +0000

    SanityHiRCAInternetExplorerTest.java edited online with Bitbucket

--- SanityTestHiRCAInternetExplorer/src/test/java/SanityHiRCAInternetExplorerTest.java
+++ SanityTestHiRCAInternetExplorer/src/test/java/SanityHiRCAInternetExplorerTest.java
@@ -25,7 +25,7 @@ public class SanityHiRCAInternetExplorerTest {
 	
 	@SuppressWarnings("deprecation")
 	@Rule
-	  public Timeout globalTimeout= new Timeout(240000);
+	  public Timeout globalTimeout= new Timeout(600000);
 	  
 	@Before
 	  public void beforeTest() throws MalformedURLException{
