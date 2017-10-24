76f9a9a92d43895606acd3e3c049f621deb341f8
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-10-24 16:18:55 +0000

    SanityIETest.java edited online with Bitbucket

--- KaleAsiaSanityTestOPiRCA/src/test/java/SanityIETest.java
+++ KaleAsiaSanityTestOPiRCA/src/test/java/SanityIETest.java
@@ -29,7 +29,7 @@ public class SanityIETest {
 
 	@SuppressWarnings("deprecation")
 	@Rule
-	  public Timeout globalTimeout= new Timeout(240000);
+	  public Timeout globalTimeout= new Timeout(600000);
 	
 	@Before
 	  public void beforeTest() throws MalformedURLException{
