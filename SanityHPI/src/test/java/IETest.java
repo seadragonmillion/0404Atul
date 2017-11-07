2f01e9a3b32f77d63e0af7401d949a974a801ecb
ritica <rramakrishnan@errorfree.com>
2017-11-07 12:58:02 -0800

    QAA 140

--- SanityHPI/src/test/java/IETest.java
+++ SanityHPI/src/test/java/IETest.java
@@ -18,7 +18,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.assertj.core.api.SoftAssertions;
 
 public class IETest {
 
@@ -28,10 +28,11 @@ public class IETest {
 	private String ie_path = "C:\\Users\\rramakrishnan\\DriversForSelenium\\IEDriverServer.exe";
 	private String url = "https://kaledev.error-free.com/";
 	private int login =0;
+	SoftAssertions softly = new SoftAssertions();
 	
 	@SuppressWarnings("deprecation")
 	@Rule
-	  public Timeout globalTimeout= new Timeout(240000);
+	  public Timeout globalTimeout= new Timeout(600000);
 	@Before
 	  public void beforeTest() throws MalformedURLException{
 		  
@@ -134,8 +135,7 @@ public class IETest {
 	public void deleteNewRecord(String recordName) throws Exception{
 		  
 		  WebDriverWait wait = new WebDriverWait(driver,10);
-		  //CLicks on first newly created record
-		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-user-home-activities-hpi']/ul/li[2]/a"))).click();
+		  
 		  //Clicks on delete button
 		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-user-home-activities-single']/div/div/a"))).click();
 		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-title")));
@@ -157,6 +157,58 @@ public class IETest {
 		  
 		  			  
 	  }
+
+
+	   public void shareReport() throws Exception{
+	    	
+	    	WebDriverWait wait1 = new WebDriverWait(driver,60);
+	    	//CLicks on first newly created record
+		    wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-user-home-activities-hpi']/ul/li[2]/a"))).click();
+			//Switches to the iframe
+			//wait1.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("pii-iframe-main"));
+	    	//Clicks on share button
+	    	wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-user-home-activities-single']/div/div/a[2]"))).click();
+			//Enters username
+			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-uhshare-search-input"))).sendKeys("qaacreator");
+	    	//Selects from dropdown
+			WebElement dropdown = wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-uhshare-blocks']/div[2]/ul")));
+			dropdown.findElement(By.cssSelector(".ui-first-child.ui-last-child")).click();
+			//Clicks on add user
+			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-title"))).click();
+			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-confirmed"))).click();
+			//Verifies user added
+			String user=wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-uhshare-blocks']/div/form/div/ul/li/a"))).getText();
+			softly.assertThat(user).as("test data").isEqualTo("qaacreator");
+			//Clicks on save
+			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-uhshare-save"))).click();
+	    }
+	    
+	    public void markCritical() throws Exception{
+	    	
+	    	WebDriverWait wait1 = new WebDriverWait(driver,60);
+	    	//Clicks on mark critical
+	    	wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-user-home-activities-single']/div[2]/div/label"))).click();
+	    	//Clicks on confirm change
+			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-title"))).click();
+			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-confirmed"))).click();
+			//Checks if marked critical
+			String critical=wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='hpi-rpt']/div/span[4]/strong"))).getText();
+			softly.assertThat(critical).as("test data").contains("Critical");
+			if(driver.findElement(By.xpath(".//*[@id='hpi-rpt']/div/span[4]/strong")).isDisplayed())
+				System.out.println("Marked critical");
+			//Clicks on mark critical again
+	    	wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-user-home-activities-single']/div[2]/div/label"))).click();
+	    	//Clicks on confirm change
+			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-title"))).click();
+			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-confirmed"))).click();
+			Thread.sleep(2000);
+			if(driver.findElement(By.xpath(".//*[@id='hpi-rpt']/div/span[4]/strong")).isDisplayed()==false)
+			{
+				System.out.println("Unmarked critical");
+			}
+				
+	    }
+	  
 	
 	@Test
 	  public void SanityTest() throws Exception{
@@ -240,8 +292,15 @@ public class IETest {
 		  	
 		  //Checks if the record name is correct
 		  assertEquals(name,recordName);
+		  //Shares report
+		  shareReport();
+		  Thread.sleep(2000);
+		  //Mark critical
+		  markCritical();
+		  Thread.sleep(2000);
 		  //Deletes the record
-		   deleteNewRecord(recordName);
+		  deleteNewRecord(recordName);
+		  Thread.sleep(2000);
 		  //Logs out
 		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-loginname"))).click();
 		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-signout-button"))).click();
