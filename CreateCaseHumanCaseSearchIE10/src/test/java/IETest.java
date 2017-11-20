73712416dbac5ea2fb194ab07204e5d59b652db3
ritica <rramakrishnan@errorfree.com>
2017-11-20 10:21:09 -0800

    added import

--- CreateCaseHumanCaseSearchIE10/src/test/java/IETest.java
+++ CreateCaseHumanCaseSearchIE10/src/test/java/IETest.java
@@ -21,7 +21,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.openqa.selenium.NoSuchElementException;
 
 public class IETest {
 
