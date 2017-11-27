421940037610a5cfaa05af3cceb86b10585e78c3
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-11-27 18:37:20 +0000

    ChromeTest.java edited online with Bitbucket

--- CreateCaseHumanCaseSearchChrome/src/test/java/ChromeTest.java
+++ CreateCaseHumanCaseSearchChrome/src/test/java/ChromeTest.java
@@ -25,8 +25,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class ChromeTest {
 
 	private WebDriver driver;
-	private String username ="ritica";
-	private String password = "Kale24982417@";
+	private String username ="jenkinsvm";
+	private String password = "Kalejenkins@123";
 	private String chrome_path = "C:\\Users\\rramakrishnan\\DriversForSelenium\\chromedriver.exe";
 	private String url = "https://kaledev.error-free.com/";
 	private int numberOfImages=5;
