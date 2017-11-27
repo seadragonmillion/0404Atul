94c39cf6951e2427f04ada9a71a5f7215e64e541
ritica <rramakrishnan@errorfree.com>
2017-11-27 13:44:11 -0800

    QAA 203 dev

--- SanityHumanCaseSearch/src/test/java/FirefoxTest.java
+++ SanityHumanCaseSearch/src/test/java/FirefoxTest.java
@@ -318,7 +318,7 @@ public class FirefoxTest {
 			  assertEquals (actual_title, expected_title);
 			  //Checking if copyright is correct
 			  String copyright_xpath = ".//*[@id='pii-slideshow-Q1459']/ul/li["+i+"]/span/span";
-			  actual_copyright = driver.findElement(By.xpath(copyright_xpath)).getText();
+			  actual_copyright = driver.findElement(By.xpath(copyright_xpath)).getAttribute("textContent");
 			  assertEquals (actual_copyright, expected_copyright);
 			  //Checking if footer image appears
 			  String image_xpath = ".//*[@id='pii-slideshow-Q1459']/ul/li["+i+"]/span/img";
@@ -326,7 +326,7 @@ public class FirefoxTest {
 				  System.out.println("Logo is displayed");
 			  //Checking if slide number appears and is correct
 			  String slide_xpath = ".//*[@id='pii-slideshow-Q1459']/ul/li["+i+"]/span/span[2]";
-			  actual_slide = driver.findElement(By.xpath(slide_xpath)).getText();
+			  actual_slide = driver.findElement(By.xpath(slide_xpath)).getAttribute("textContent");
 			  expected_slide = i+"/"+n;
 			  assertEquals (actual_slide, expected_slide);
 			  //Click on Previous
@@ -429,7 +429,7 @@ public class FirefoxTest {
 			  assertEquals (actual_title, expected_title);
 			  //Checking if copyright is correct
 			  String copyright_xpath = ".//*[@id='pii-slideshow-Q1459']/ul/li["+i+"]/span/span";
-			  actual_copyright = driver.findElement(By.xpath(copyright_xpath)).getText();
+			  actual_copyright = driver.findElement(By.xpath(copyright_xpath)).getAttribute("textContent");
 			  assertEquals (actual_copyright, expected_copyright);
 			  //Checking if footer image appears
 			  String image_xpath = ".//*[@id='pii-slideshow-Q1459']/ul/li["+i+"]/span/img";
@@ -437,7 +437,7 @@ public class FirefoxTest {
 				  System.out.println("Logo is displayed");
 			  //Checking if slide number appears and is correct
 			  String slide_xpath = ".//*[@id='pii-slideshow-Q1459']/ul/li["+i+"]/span/span[2]";
-			  actual_slide = driver.findElement(By.xpath(slide_xpath)).getText();
+			  actual_slide = driver.findElement(By.xpath(slide_xpath)).getAttribute("textContent");
 			  expected_slide = i+"/"+n;
 			  assertEquals (actual_slide, expected_slide);
 			  //Click on Previous
