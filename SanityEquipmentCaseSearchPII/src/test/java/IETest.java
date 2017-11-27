02fbbd596dd418d531ef7cfe90d1231ba2206fe1
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2017-11-27 21:52:50 +0000

    IETest.java edited online with Bitbucket

--- SanityEquipmentCaseSearchPII/src/test/java/IETest.java
+++ SanityEquipmentCaseSearchPII/src/test/java/IETest.java
@@ -317,7 +317,7 @@ public class IETest {
 		  //Click on previous
 		  driver.findElement(By.linkText("Previous")).click();
 		  //Checks for previous button
-		  for (int i=n;i<=2;i--)
+		  for (int i=n;i>=2;i--)
 		  {
 			  String id = "pii-slideimg-F1459-"+(i-1);
 			  Thread.sleep(1000);
@@ -431,7 +431,7 @@ public class IETest {
 		  //Click on previous
 		  driver.findElement(By.linkText("Previous")).click();
 		  //Checks for previous button
-		  for (int i=n;i<=2;i--)
+		  for (int i=n;i>=2;i--)
 		  {
 			  String id = "pii-slideimg-F1459-"+(i-1);
 			  Thread.sleep(1000);
