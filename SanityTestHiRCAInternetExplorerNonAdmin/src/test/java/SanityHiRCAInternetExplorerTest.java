502b8967be6c6c36cf053cf8f8a01fd48e4f0662
ritica <rramakrishnan@errorfree.com>
2017-12-13 10:00:25 -0800

    test failures

--- SanityTestHiRCAInternetExplorerNonAdmin/src/test/java/SanityHiRCAInternetExplorerTest.java
+++ SanityTestHiRCAInternetExplorerNonAdmin/src/test/java/SanityHiRCAInternetExplorerTest.java
@@ -140,7 +140,7 @@ public class SanityHiRCAInternetExplorerTest {
 		  Thread.sleep(2000);
 		  jse.executeScript("return document.getElementById('pii-user-home-panel-btn-irca').click();");
 		  //Verify record deleted
-		  Thread.sleep(1000);
+		  Thread.sleep(4000);
 		  //Click on 1st record
 		  String name = driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-irca']/ul/li[2]/a")).getText();
 		  System.out.println(name);
