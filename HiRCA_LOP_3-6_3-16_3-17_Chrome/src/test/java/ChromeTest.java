0e66df94c3f16ef92f59d033d335b8c1fdf1f90e
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2018-01-05 18:41:39 +0000

    ChromeTest.java edited online with Bitbucket

--- HiRCA_LOP_3-6_3-16_3-17_Chrome/src/test/java/ChromeTest.java
+++ HiRCA_LOP_3-6_3-16_3-17_Chrome/src/test/java/ChromeTest.java
@@ -86,7 +86,7 @@ public class ChromeTest {
 	          chromeOptionsMap.put("plugins.plugins_disabled", new String[] {
 	        		    "Chrome PDF Viewer"
 	        		});
-	          String downloadFilepath = "C:\\Users\\rramakrishnan\\Downloads\\reports";
+	          String downloadFilepath = "C:\\Users\\IEUser\\Downloads\\reports";
 	          chromeOptionsMap.put("download.default_directory", downloadFilepath);
 	          driver = new ChromeDriver(cap);
 			  //Browser is maximized
@@ -224,7 +224,7 @@ public class ChromeTest {
 		    public void downloadRecord(String executive,String event_id,String text184, String text, String paragraph_investigators,String paragraph_background,String paragraph_timeline,String paragraph_problem, String get_date, String get_time, String get_dept, String creationDate) throws Exception {
 		    	
 		    	//deletes files in reports folder before starting to download
-		    	File file = new File("C://Users//rramakrishnan//Downloads//reports//");
+		    	File file = new File("C://Users//IEUser//Downloads//reports//");
 		    	deleteFiles(file);
 		    	WebDriverWait wait1 = new WebDriverWait(driver,60);
 		    	//Clicks on first newly created record
@@ -257,7 +257,7 @@ public class ChromeTest {
 		    	
 		    	 List<String> results = new ArrayList<String>();
 		    	//Gets the file name which has been downloaded
-		    	File[] files = new File("C://Users//rramakrishnan//Downloads//reports//").listFiles();
+		    	File[] files = new File("C://Users//IEUser//Downloads//reports//").listFiles();
 		    	//If this pathname does not denote a directory, then listFiles() returns null. 
 		    	for (File file : files) {
 		    	    if (file.isFile()) {
@@ -266,7 +266,7 @@ public class ChromeTest {
 		    	}
 		    	System.out.println(results.get(0));
 		    	//Loads the file to check if correct data is present
-		    	String fileName="C://Users//rramakrishnan//Downloads//reports//"+results.get(0);
+		    	String fileName="C://Users//IEUser//Downloads//reports//"+results.get(0);
 		    	File file = new File(fileName);
 		    	FileInputStream fis = new FileInputStream(file);
 		    	PDFParser parser = new PDFParser(fis);
