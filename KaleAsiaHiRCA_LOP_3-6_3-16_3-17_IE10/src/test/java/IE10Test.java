d196870add9f4893a173c43744a473e4f5e6819d
Ritica Ramakrishnan <rramakrishnan@errorfree.com>
2018-01-08 17:19:55 +0000

    IE10Test.java edited online with Bitbucket

--- KaleAsiaHiRCA_LOP_3-6_3-16_3-17_IE10/src/test/java/IE10Test.java
+++ KaleAsiaHiRCA_LOP_3-6_3-16_3-17_IE10/src/test/java/IE10Test.java
@@ -1453,7 +1453,7 @@ public class IE10Test {
 			  driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
 			  Thread.sleep(8000);
 			  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			  Thread.sleep(4000);
+			  Thread.sleep(8000);
 			  try{
 	               if (login==1)
 	               {
