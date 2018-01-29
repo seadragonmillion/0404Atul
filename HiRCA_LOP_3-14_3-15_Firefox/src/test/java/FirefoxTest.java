edc86771b6b31b7d17548f19408c2e97535c8992
ritica <rramakrishnan@errorfree.com>
2018-01-29 14:13:12 -0800

    changes to 3.15 3.14 dev

--- HiRCA_LOP_3-14_3-15_Firefox/src/test/java/FirefoxTest.java
+++ HiRCA_LOP_3-14_3-15_Firefox/src/test/java/FirefoxTest.java
@@ -72,7 +72,7 @@ public class FirefoxTest {
 	 private String reason19="Malls are great places to shop; I can find everything I need under one roof.";
 	 private String reason20="She wrote him a long letter, but he didn't read it.";
 	 private String reason21="She works two jobs to make ends meet; at least, that was her reason for not having time to join us.";
-	 private String reason22="What was the person thinking when they discovered cow�s milk was fine for human consumption, and why did they do it in the first place!?";
+	 private String reason22="What was the person thinking when they discovered cow's milk was fine for human consumption, and why did they do it in the first place!?";
 	 private String reason23="Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play.";
 	 private String[] lopOptions1=new String[10];
 	 private String[] lopOptions2=new String[10];
