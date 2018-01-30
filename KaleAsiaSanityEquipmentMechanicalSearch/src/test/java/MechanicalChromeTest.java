d3a3201fde4e694a3bfcd1c8b429b4a5ccdb20ed
ritica <rramakrishnan@errorfree.com>
2018-01-30 09:47:11 -0800

    mech chrome asia us synchronization

--- KaleAsiaSanityEquipmentMechanicalSearch/src/test/java/MechanicalChromeTest.java
+++ KaleAsiaSanityEquipmentMechanicalSearch/src/test/java/MechanicalChromeTest.java
@@ -190,6 +190,7 @@ public class MechanicalChromeTest {
 		  driver.findElement(By.id("pii-efse-searchbykw-input")).sendKeys(Keys.ENTER);
 		  //Waits for black loading message to disappear
 		  try{
+			  Thread.sleep(1000);
 			  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("ui-icon-loading")));
 			  wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("ui-icon-loading")));
 			 }catch (org.openqa.selenium.TimeoutException e)
