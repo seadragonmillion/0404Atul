ada5d1e97a14509b4bfcd675af2c76295fb3bc31
ritica <rramakrishnan@errorfree.com>
2018-02-05 13:24:20 -0800

    asia equip create chrome

--- KaleAsiaCreateCaseEquipmentCaseChrome/src/test/java/ChromeTest.java
+++ KaleAsiaCreateCaseEquipmentCaseChrome/src/test/java/ChromeTest.java
@@ -814,6 +814,7 @@ public class ChromeTest {
 		 try{ 
 		  Login();
 		  System.out.println("Title after login: "+driver.getTitle());
+		  Thread.sleep(5000);
 		  //Waits for the page to load
 	      driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		  //Switches to the iframe
