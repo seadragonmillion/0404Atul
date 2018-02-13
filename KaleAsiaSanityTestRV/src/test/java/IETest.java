275b9166ca0d4ed339b8ff062d6ceb1710b97cb4
ritica <rramakrishnan@errorfree.com>
2018-02-13 09:16:25 -0800

    asia us rv

--- KaleAsiaSanityTestRV/src/test/java/IETest.java
+++ KaleAsiaSanityTestRV/src/test/java/IETest.java
@@ -223,7 +223,7 @@ public class IETest {
 			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-title"))).click();
 			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-user-home-dialog-confirmed"))).click();
 			//Verifies user added
-			String user=wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-uhshare-blocks']/div/form/div/ul/li[2]/a"))).getText();
+			String user=wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='pii-uhshare-blocks']/div/form/div/ul/li/a"))).getText();
 			softly.assertThat(user).as("test data").isEqualTo("qaacreator");
 			//Clicks on save
 			wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-uhshare-save"))).click();
