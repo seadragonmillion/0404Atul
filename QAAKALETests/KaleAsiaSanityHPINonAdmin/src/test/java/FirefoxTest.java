c3743b649cf9331ea6f1012f53be4f5c89c25cd8
ritica <rramakrishnan@errorfree.com>
2018-05-01 14:12:34 -0700

    hpi non admin

---
+++ QAAKALETests/KaleAsiaSanityHPINonAdmin/src/test/java/FirefoxTest.java
@@ -0,0 +1,121 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FirefoxTest {
+
+	private FirefoxDriver driver;
+	private String username ="qaacfi";
+	private String password = "S2FsZTk0OTM1ODMwQA==";
+	private String gecko_path = "C:\\Users\\rramakrishnan\\DriversForSelenium\\geckodriver.exe";
+	private String url = "https://kaleasia.error-free.com/";
+	
+	@Before
+	  public void beforeTest() throws MalformedURLException{
+		  
+		 System.out.println("Performing sanity test on HPI in Firefox Non Admin");
+		 System.setProperty("webdriver.gecko.driver",gecko_path);
+		 ProfilesIni ffProfiles = new ProfilesIni();
+		 FirefoxProfile profile = ffProfiles.getProfile("customFirefox");
+		 FirefoxOptions options = new FirefoxOptions();
+		 options.setCapability(FirefoxDriver.PROFILE, profile);
+		 driver = new FirefoxDriver(options);
+		 Dimension initialSize= driver.manage().window().getSize();
+		 System.out.println(initialSize);
+		 int height=initialSize.getHeight();
+		 if(height<1900)
+		 {
+			//Browser is maximized
+			driver.manage().window().maximize(); 
+		 }
+		 Dimension finalSize=driver.manage().window().getSize();
+		 System.out.println(finalSize);
+		  //Browser navigates to the KALE url
+		  driver.navigate().to(url);
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  }
+
+	@Test
+	  public void SanityTest() throws Exception{
+		  
+		  Login obj = new Login();
+		  HPI obj1 = new HPI();
+		  //Logs in
+		  int login = obj.LoginUser(driver, username, password);
+		  System.out.println("Title after login: "+driver.getTitle());
+		  Thread.sleep(4000);
+		  //Waits for the page to load
+	      driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		  //Switches to the iframe
+		  driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
+		  try{
+             if (login==1)
+             {
+                   WebDriverWait wait2 = new WebDriverWait(driver,20);
+                   wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-close"))).click();
+             }
+      }catch (NoSuchElementException e){
+             throw e;
+      }
+		  Thread.sleep(10000);
+		  WebDriverWait wait = new WebDriverWait(driver,20);
+		  //Clicks on Analysis 
+		  try
+		  {
+			  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-main-menu-button-a"))).click();
+		  }catch (UnhandledAlertException f){			  
+			  driver.switchTo().alert().dismiss();
+		  }
+		  obj1.createReport (driver,username);
+		  //Gets the name of the record created
+		  WebElement record = driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-hpi']/ul/li[2]/a"));
+		  String recordName = record.getText();
+		  //Shares report
+		  /* Dev/Asia
+		 * 0=admin
+		 * 1=non admin
+		 * 2= admin ie11
+		 * 3= non admin ie11
+		 * US
+		 * 4=admin
+		 * 5=non admin
+		 * 6=admin ie11
+		 * 7=non admin ie11
+		 */
+		  obj1.shareReport(driver, username, password, 1);
+		  Thread.sleep(2000);
+		  //Mark critical
+		  obj1.markCritical(driver);
+		  Thread.sleep(2000);
+		  //Deletes the record
+		   obj1.deleteNewRecord(driver, recordName);
+		   Thread.sleep(2000);
+		  //Logs out
+		  obj.logout(driver);
+		  afterTest(obj1);
+		   
+	}
+	
+	public void afterTest(HPI obj1) throws Exception{
+		
+		driver.manage().window().maximize();
+		//Browser closes
+		driver.quit();
+		obj1.softAssert();
+	}
+
+
+}
---
+++ QAAKALETests/SanityHPINonAdmin/src/test/java/IETest.java
@@ -0,0 +1,106 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class IETest {
+
+	private InternetExplorerDriver driver;
+	private String username ="jenkinsvmnonadmin";
+	private String password = "S2FsZWplbmtpbnNAMTIz";
+	private String ie_path = "C:\\Users\\rramakrishnan\\DriversForSelenium\\IEDriverServer.exe";
+	private String url = "https://kaledev.error-free.com/";
+	
+	@Before
+	  public void beforeTest() throws MalformedURLException{
+		  
+		  System.out.println("Performing sanity test on HPI in Internet Explorer Non Admin ");
+		  System.setProperty("webdriver.ie.driver",ie_path);
+		  DesiredCapabilities cap = new DesiredCapabilities(); 
+		  cap.setCapability("ignoreZoomSettings", true);
+		  cap.setCapability("requireWindowFocus", true);
+		  driver = new InternetExplorerDriver(cap);
+		  //Browser is maximized
+		  driver.manage().window().maximize();
+		  //Browser navigates to the KALE url
+		  driver.navigate().to(url);
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  }
+	
+	@Test
+	  public void SanityTest() throws Exception{
+		  
+		  Login obj = new Login();
+		  HPI obj1 = new HPI();
+		  //Logs in
+		  int login = obj.LoginUser(driver, username, password);
+		  System.out.println("Title after login: "+driver.getTitle());
+		  Thread.sleep(4000);
+		  //Waits for the page to load
+	      driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		  //Switches to the iframe
+		  driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
+		  try{
+           if (login==1)
+           {
+                 WebDriverWait wait2 = new WebDriverWait(driver,20);
+                 wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-close"))).click();
+           }
+    }catch (NoSuchElementException e){
+           throw e;
+    }
+		  Thread.sleep(10000);
+		  WebDriverWait wait = new WebDriverWait(driver,20);
+		  //Clicks on Analysis 
+		  try
+		  {
+			  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-main-menu-button-a"))).click();
+		  }catch (UnhandledAlertException f){			  
+			  driver.switchTo().alert().dismiss();
+		  }
+		  obj1.createReport (driver,username);
+		  //Gets the name of the record created
+		  WebElement record = driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-hpi']/ul/li[2]/a"));
+		  String recordName = record.getText();
+		  //Shares report
+		  /* Dev/Asia
+		 * 0=admin
+		 * 1=non admin
+		 * 2= admin ie11
+		 * 3= non admin ie11
+		 * US
+		 * 4=admin
+		 * 5=non admin
+		 * 6=admin ie11
+		 * 7=non admin ie11
+		 */
+		  obj1.shareReport(driver, username, password, 1);
+		  Thread.sleep(2000);
+		  //Mark critical
+		  obj1.markCritical(driver);
+		  Thread.sleep(2000);
+		  //Deletes the record
+		   obj1.deleteNewRecord(driver, recordName);
+		   Thread.sleep(2000);
+		  //Logs out
+		  obj.logout(driver);
+		  afterTest(obj1);
+		   
+	}
+	
+	public void afterTest(HPI obj1) throws Exception{
+		
+		//Browser closes
+		driver.quit();
+		obj1.softAssert();
+	}
+}
