1f35e4fe78656dc1d1e897fc5c5daa3546d68e79
ritica <rramakrishnan@errorfree.com>
2018-05-03 10:56:38 -0700

    chrome ef pdd non admin case search increased time

--- QAAKALETests/SanityPreventionDesignDeficienciesNonAdmin/src/test/java/ChromeTest.java
+++ QAAKALETests/SanityPreventionDesignDeficienciesNonAdmin/src/test/java/ChromeTest.java
@@ -38,9 +38,10 @@ public class ChromeTest {
 		EquipmentPDDandEF obj1 = new EquipmentPDDandEF ();
 		int login = obj.LoginUser(driver, username, password);
 		System.out.println("Title after login: "+driver.getTitle());
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 		//Waits for the page to load
 	    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	    Thread.sleep(5000);
 		//Switches to the iframe
 		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
 		try{
