dc9dde963f5e155440618706fbc50dbc5a193279
ritica <rramakrishnan@errorfree.com>
2018-05-03 10:53:01 -0700

    firefox ef and pdd modules changed firefox startup

--- QAAKALETests/SanityPreventionDesignDeficiencies/src/test/java/FirefoxTest.java
+++ QAAKALETests/SanityPreventionDesignDeficiencies/src/test/java/FirefoxTest.java
@@ -29,10 +29,12 @@ public class FirefoxTest {
 		  System.out.println("Case Search for PDD in Firefox");
 		  System.setProperty("webdriver.gecko.driver",gecko_path);
 		  ProfilesIni ffProfiles = new ProfilesIni();
-		  FirefoxProfile profile = ffProfiles.getProfile("customFirefox");
-		  FirefoxOptions options = new FirefoxOptions();
-		  options.setCapability(FirefoxDriver.PROFILE, profile);
-		  driver = new FirefoxDriver(options);
+		 FirefoxProfile profile = ffProfiles.getProfile("HiRCAEvent");
+		 profile.setPreference("browser.download.folderList", 2);
+		 profile.setPreference("browser.download.dir", "C:\\Users\\IEUser\\Downloads\\reports");
+		 FirefoxOptions options = new FirefoxOptions();
+		 options.setCapability(FirefoxDriver.PROFILE, profile);
+		 driver = new FirefoxDriver(options);
 		  Dimension initialSize= driver.manage().window().getSize();
 		  System.out.println(initialSize);
 		  int height=initialSize.getHeight();
