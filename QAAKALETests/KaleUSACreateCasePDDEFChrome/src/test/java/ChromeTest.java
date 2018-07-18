98bf9c5c83debd89114f35518391fa0e5e536922
ritica <rramakrishnan@errorfree.com>
2018-07-18 13:13:29 -0700

    pdd ef create cases expanded test for keyword and title search

--- QAAKALETests/KaleUSACreateCasePDDEFChrome/src/test/java/ChromeTest.java
+++ QAAKALETests/KaleUSACreateCasePDDEFChrome/src/test/java/ChromeTest.java
@@ -35,11 +35,12 @@ public class ChromeTest {
 	@Test
 	public void test() throws Exception {
 		Login obj = new Login ();
-		EquipmentCaseLoad obj1 = new EquipmentCaseLoad ();
+		CreateEquipmentCase obj1 = new CreateEquipmentCase ();
 		EquipmentPDDandEF obj2 = new EquipmentPDDandEF ();
+		ShareCheck obj3 = new ShareCheck();
 		int login = obj.LoginUser(driver, username, password);
 		System.out.println("Title after login: "+driver.getTitle());
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 		//Waits for the page to load
 	    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		//Switches to the iframe
@@ -54,25 +55,18 @@ public class ChromeTest {
                throw e;
         }
 		Thread.sleep(5000);
-		WebDriverWait wait = new WebDriverWait(driver,20);
 		//Waits for black loading message
-		try{
-			  wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("ui-icon-loading")));
-			  wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("ui-icon-loading")));
-			 }catch (org.openqa.selenium.TimeoutException e)
-			  {
-				  
-			  }
+		obj3.loadingServer(driver);
 		//Deletes any previous cases if any for Prevention of Design Deficiencies
-	    obj1.deletePreviousCase(driver, obj2.keywordPDD);
-	    //Go to KALE homepage
-	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("pii-logo-div-element-kale"))).click();
+	    obj1.deletePreviousCase(driver, obj2.titlePDDUS);
 	    //Deletes any previous cases if any for Engineering Fundamentals
-	    obj1.deletePreviousCase(driver, obj2.keywordEF);
+	    obj1.deletePreviousCase(driver, obj2.titleEFUS);
 	    //Create one case for Prevention of Design Deficiencies
-	    String casePDD = obj2.createCaseChrome(driver, obj2.keywordPDD, obj2.titlePDD);
+	    List<String> casePDD = obj2.createCaseChrome(driver, obj2.keywordPDDUS, obj2.key1PDDUS, obj2.key2PDDUS, obj2.key3PDDUS, obj2.titlePDDUS);
+	    System.out.println("Cases for Design "+casePDD);
 	    //Create one case for Engineering Fundamentals
-	    String caseEF = obj2.createCaseChrome(driver, obj2.keywordEF, obj2.titleEF);
+	    List<String> caseEF = obj2.createCaseChrome(driver, obj2.keywordEFUS, obj2.key1EFUS, obj2.key2EFUS, obj2.key3EFUS, obj2.titleEFUS);
+	    System.out.println("Cases for Fundamentals "+caseEF);
 	    Thread.sleep(2000);
 	    //Verify if names of case modules correct
 	    obj2.verifyCaseModuleNames(driver);
@@ -81,41 +75,41 @@ public class ChromeTest {
 	    obj2.getEquipPerformancePIILink(driver);
 	    Thread.sleep(2000);
 	    //Browse for Prevention of Design Deficiencies case in Equipment PII
-	    obj2.browseCase(driver, obj2.keywordPDD, casePDD, obj2.titlePDD);
-	    //Go to KALE homepage
-	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("pii-logo-div-element-kale"))).click();
+	    obj2.browseCase(driver, obj2.keywordPDDUS, casePDD.get(0), obj2.titlePDDUS);
 	    Thread.sleep(2000);
 	    //Go to Equipment PII link
 	    obj2.getEquipPerformancePIILink(driver);
 	    //Browse for Engineering Fundamentals case in Equipment PII
-	    obj2.browseCase(driver, obj2.keywordEF, caseEF, obj2.titleEF);
+	    obj2.browseCase(driver, obj2.keywordEFUS, caseEF.get(0), obj2.titleEFUS);
 	    Thread.sleep(2000);
-	    //Go to KALE homepage
-	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("pii-logo-div-element-kale"))).click();	    
 	    //Go to Prevention of Design Deficiencies link
 	    obj2.getPDDLink(driver);
 	    //Browse for Prevention of Design Deficiencies case in PDD
-	    obj2.browseCase(driver, obj2.keywordPDD, casePDD, obj2.titlePDD);
-	    //Go to KALE homepage
-	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("pii-logo-div-element-kale"))).click();	    
+	    obj2.browseCase(driver, obj2.keywordPDDUS, casePDD.get(0), obj2.titlePDDUS);
 	    //Go to Engineering Fundamentals link
 	    obj2.getEFLink(driver);
 	    //Browse for Engineering Fundamentals case in EF
-	    obj2.browseCase(driver, obj2.keywordEF, caseEF, obj2.titleEF);
-	    //Search for case in Equipment non pii by keyword
-		obj2.searchCaseInEquipSearchByKeyword(driver, obj2.keywordPDD, casePDD);
-		//Search for case in Equipment non pii by case id
-		obj2.searchCaseInEquipSearchByCaseID(driver, casePDD);
-		//Search for case in Equipment non pii by keyword
-		obj2.searchCaseInEquipSearchByKeyword(driver, obj2.keywordEF, caseEF);
-		//Search for case in Equipment non pii by case id
-		obj2.searchCaseInEquipSearchByCaseID(driver, caseEF);
+	    obj2.browseCase(driver, obj2.keywordEFUS, caseEF.get(0), obj2.titleEFUS);
+	    //Search for case in Equipment performance by keyword
+		obj2.searchCaseInEquipSearchByKeyword(driver, obj2.keywordPDDUS, casePDD.get(0));
+		//Search for case in Equipment performance by case id
+		obj2.searchCaseInEquipSearchByCaseID(driver, casePDD.get(0));
+		//Search for case in Equipment performance by keyword
+		obj2.searchCaseInEquipSearchByKeyword(driver, obj2.keywordEFUS, caseEF.get(0));
+		//Search for case in Equipment performance by case id
+		obj2.searchCaseInEquipSearchByCaseID(driver, caseEF.get(0));
 		//Search for PDD and EF case in Failure Modes
-		obj2.searchCaseInFailureModes(driver, caseEF, casePDD);
+		obj2.searchCaseInFailureModes(driver, caseEF.get(0), casePDD.get(0));
+		//Check keyword and cases in EF
+		obj1.checkkeyword(driver, caseEF, obj2.keywordEFUS, obj2.key1EFUS, obj2.key2EFUS, obj2.key3EFUS, 2, obj2.titleEFUS);
+		//Check keyword and cases in PDD
+		obj1.checkkeyword(driver, casePDD, obj2.keywordPDDUS, obj2.key1PDDUS, obj2.key2PDDUS, obj2.key3PDDUS, 2, obj2.titlePDDUS);
 	    //Delete Prevention of Design Deficiencies case
 	    obj1.deleteCase(driver, casePDD);
+	    System.out.println("Cases deleted for Design");
 	    //Delete Engineering Fundamentals case
 	    obj1.deleteCase(driver, caseEF);
+	    System.out.println("Cases deleted for Fundamentals");
 	    //Logout
 	    obj.logout(driver);
 		afterTest(obj2);		
