220cde18c967dc626b7be92b7b834d59e4612b7c
ritica <rramakrishnan@errorfree.com>
2018-09-07 15:47:36 -0700

    hirca human error level 1

---
+++ QAAKALETests/KaleUSAHiRCAHumanErrorLevel1/src/test/java/FirefoxTest.java
@@ -0,0 +1,87 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class FirefoxTest {
+
+	private WebDriver driver;
+	private String username = "jenkinsvmnonadmin";
+	private String password = "S2FsZWplbmtpbnNAMTIz";
+	private String gecko_path = "C:\\Users\\rramakrishnan\\DriversForSelenium\\geckodriver.exe";
+	private String url = "https://kale.error-free.com/";
+
+	@Before
+	public void beforeTest() throws MalformedURLException{
+
+		System.out.println("HiRCA Level1 path for Human Error in Firefox");
+		System.setProperty("webdriver.gecko.driver",gecko_path);
+		ProfilesIni ffProfiles = new ProfilesIni();
+		FirefoxProfile profile = ffProfiles.getProfile("HiRCAEvent");
+		profile.setPreference("browser.download.folderList", 2);
+		profile.setPreference("browser.download.dir", "C:\\Users\\IEUser\\Downloads\\reports");
+		FirefoxOptions options = new FirefoxOptions();
+		options.setCapability(FirefoxDriver.PROFILE, profile);
+		driver = new FirefoxDriver(options);
+		Dimension initialSize= driver.manage().window().getSize();
+		System.out.println(initialSize);
+		int height=initialSize.getHeight();
+		if(height<950)
+		{
+			//Browser is maximized
+			driver.manage().window().maximize(); 
+		}
+		Dimension finalSize=driver.manage().window().getSize();
+		System.out.println(finalSize);
+		//Browser navigates to the KALE url
+		driver.navigate().to(url);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	@Test
+	public void test() throws Exception{
+		Login obj = new Login ();
+		HiRCAHumanError obj1 = new HiRCAHumanError();
+		int login = obj.LoginUser(driver, username, password);
+		System.out.println("Title after login: "+driver.getTitle());
+		Thread.sleep(5000);
+		//Waits for the page to load
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		//Switches to the iframe
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
+		try{
+			if (login==1)
+			{
+				WebDriverWait wait2 = new WebDriverWait(driver,20);
+				wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-close"))).click();
+			}
+		}catch (NoSuchElementException |org.openqa.selenium.TimeoutException e){
+
+		}
+		Thread.sleep(5000);
+		//Path to follow
+		obj1.pathHiRCA(driver, username);
+		//Logout
+		obj.logout(driver);
+		afterTest(obj1);
+	}
+
+	public void afterTest(HiRCAHumanError obj) throws Exception {
+
+		driver.manage().window().maximize(); 
+		driver.quit();
+		obj.softAssert();
+
+	}
+}
