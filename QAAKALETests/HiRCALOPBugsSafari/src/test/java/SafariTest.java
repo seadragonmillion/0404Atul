c47f40e1a0533eebc04d7bd92443604b589fab6a
ritica <rramakrishnan@errorfree.com>
2019-02-11 17:00:41 -0800

    hirca lop for safari first test

---
+++ QAAKALETests/HiRCALOPBugsSafari/src/test/java/SafariTest.java
@@ -0,0 +1,90 @@
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.safari.SafariDriver;
+
+
+public class SafariTest {
+
+	private WebDriver driver;
+	private String username = "ritica_only_nonadmin";
+	private String password = "S2FsZWplbmtpbnNAMTIz";
+	   
+		@Before
+		  public void beforeTest() throws MalformedURLException{
+			  
+			  System.out.println("HiRCA LOP Bug Check Safari on MAC");
+			  WebDriver driver = new SafariDriver();
+			  //Browser is maximized
+			  driver.manage().window().maximize();
+			  //Browser navigates to the KALE url
+			  driver.navigate().to("https://kaleqa.error-free.com/");
+			  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		  }
+	
+	@Test
+	public void test() throws Exception{
+		Login obj = new Login ();
+		HiRCALOPBug obj1 = new HiRCALOPBug();
+		int login = obj.LoginUser(driver, username, password);
+		System.out.println("Title after login: "+driver.getTitle());
+		Thread.sleep(5000);
+		//Waits for the page to load
+	    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		//Switches to the iframe
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
+		Thread.sleep(5000);
+		if (login==1)
+		{
+          
+          while(true)
+		  {
+         	 Thread.sleep(1000);
+			  if (driver.findElement(By.cssSelector(".sticky.border-top-right.sticky-error")).isDisplayed())
+			  {
+				  WebElement ele =driver.findElement(By.cssSelector(".sticky.border-top-right.sticky-error"));
+				  ele.findElement(By.className("sticky-close")).click();
+				  break;
+			  }
+			  else break;
+		  }
+        }	
+		Thread.sleep(4000);
+		//Fill mandatory data
+		obj1.fillUpHiRCAEventInfo(driver,obj1.text);
+		//Bug KALE 1959
+		obj1.bugPath1KALE1959(driver);
+		//Bug KALE 1926
+		obj1.bugPathWith2LopsKALE1926(driver);
+		//Bug KALE 1947
+		obj1.bugPath1KALE1947(driver);
+		//Bug KALE 1957
+		obj1.bugKALE1957(driver);
+		//Bug KALE 1846
+		obj1.bugKALE1846(driver);
+		//Bug KALE 1852
+		obj1.bugKALE1852(driver);
+		//Bug KALE 2219 and KALE 2246
+		//obj1.bugKALE2219KALE2246(driver);
+		//Logout
+		obj.logout(driver);
+		afterTest(obj1);
+	}
+	
+	 public void afterTest(HiRCALOPBug obj1) throws Exception {
+		  
+		  driver.quit();
+		  obj1.softAssert();
+		  
+	  }
+
+}
