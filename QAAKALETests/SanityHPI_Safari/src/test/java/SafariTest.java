55aa86a05ed08bafb5353a0d7931d0a6a33041b3
ritica <rramakrishnan@errorfree.com>
2019-03-06 10:39:44 -0800

    hirca event info safari javascript executor click on add new supporting file button and hpi safari test

---
+++ QAAKALETests/SanityHPI_Safari/src/test/java/SafariTest.java
@@ -0,0 +1,101 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SafariTest {
+
+	private WebDriver driver;
+	private String username ="ritica_only_nonadmin";
+	private String password = "S2FsZWplbmtpbnNAMTIz";
+	private String url = "https://kaleqa.error-free.com/";
+	
+	@Before
+	  public void beforeTest() throws MalformedURLException{
+		  
+		  System.out.println("Performing sanity test on HPI in Safari on MAC");
+		System.setProperty("webdriver.safari.driver", "/Applications/Safari Technology Preview.app/Contents/MacOS/safaridriver");
+		SafariOptions options = new SafariOptions();
+		options.setUseTechnologyPreview(true);
+		driver = new SafariDriver(options);
+		//Browser is maximized
+		driver.manage().window().maximize();
+		//Browser navigates to the KALE url
+		driver.navigate().to(url);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	  }	
+	
+	@Test
+	  public void SanityTest() throws Exception{
+		  
+		  Login obj = new Login();
+		  HPI obj1 = new HPI();
+		  //Logs in
+		  int login = obj.LoginUser(driver, username, password);
+		  System.out.println("Title after login: "+driver.getTitle());
+		  Thread.sleep(4000);
+		  //Switches to the iframe
+		  driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
+		  try{
+               if (login==1)
+               {
+                     WebDriverWait wait2 = new WebDriverWait(driver,20);
+                     wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-close"))).click();
+               }
+        }catch (NoSuchElementException e){
+               throw e;
+        }
+		  Thread.sleep(10000);
+		  WebDriverWait wait = new WebDriverWait(driver,20);
+		  //Clicks on Analysis 
+		  try
+		  {
+			  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pii-main-menu-button-a"))).click();
+		  }catch (UnhandledAlertException f){			  
+			  driver.switchTo().alert().dismiss();
+		  }
+		  obj1.createReport (driver,username);
+		  //Gets the name of the record created
+		  WebElement record = driver.findElement(By.xpath(".//*[@id='pii-user-home-activities-hpi']/ul/li[2]/a"));
+		  String recordName = record.getText();
+		  //Shares report
+		  /* Dev/Asia
+		 * 0=admin
+		 * 1=non admin
+		 * 2= admin ie11
+		 * 3= non admin ie11
+		 * US
+		 * 4=admin
+		 * 5=non admin
+		 * 6=admin ie11
+		 * 7=non admin ie11
+		 */
+		  obj1.shareReport(driver, username, password, 1);
+		  Thread.sleep(2000);
+		  //Mark critical integer same as shareReport
+		  obj1.markCritical(driver,username, password,1);
+		  //Deletes the newly created record, integer same as shareReport
+		  obj1.deleteNewRecord(driver, recordName,1);
+		   Thread.sleep(2000);
+		  //Logs out
+		  obj.logout(driver);
+		  afterTest(obj1);		   
+	}
+	
+	public void afterTest(HPI obj1)throws Exception{
+		
+		//Browser closes
+		driver.quit();
+		obj1.softAssert();
+	}		   
+}
