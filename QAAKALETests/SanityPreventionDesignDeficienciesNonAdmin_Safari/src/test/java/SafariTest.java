446c13caa75a36df580c22bc3ed3451a713dd464
ritica <rramakrishnan@errorfree.com>
2019-04-05 16:55:42 -0700

    safari changed the usernames for pdd non admin test

--- QAAKALETests/SanityPreventionDesignDeficienciesNonAdmin_Safari/src/test/java/SafariTest.java
+++ QAAKALETests/SanityPreventionDesignDeficienciesNonAdmin_Safari/src/test/java/SafariTest.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class SafariTest {
 
 	private WebDriver driver;
-	private String username ="ritica_only_nonadmin";
+	private String username ="qaasafari_nonadmin";
 	private String password = "S2FsZWplbmtpbnNAMTIz";
 	private String url = System.getProperty("qaurl");
 
