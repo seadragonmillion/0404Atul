98a9fc3be4ffbacb71b66bde0a13e02d74a52a2f
ritica <rramakrishnan@errorfree.com>
2019-06-14 09:59:37 -0700

    hirca lop test modified and qaa 728

--- QAAKALETests/HiRCALOPBugsSafari/src/test/java/SafariTest.java
+++ QAAKALETests/HiRCALOPBugsSafari/src/test/java/SafariTest.java
@@ -38,12 +38,8 @@ public class SafariTest {
 		HiRCALOPBug obj1 = new HiRCALOPBug();
 		int login = obj.LoginUser(driver, username, password);
 		System.out.println("Title after login: "+driver.getTitle());
-		Thread.sleep(5000);
-		//Waits for the page to load
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		//Switches to the iframe
 		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
-		Thread.sleep(5000);
 		if (login==1)
 		{
 
@@ -59,23 +55,8 @@ public class SafariTest {
 				else break;
 			}
 		}	
-		Thread.sleep(4000);
 		//Fill mandatory data
-		obj1.fillUpHiRCAEventInfo(driver,obj1.text);
-		//Bug KALE 1959
-		obj1.bugPath1KALE1959(driver);
-		//Bug KALE 1926
-		obj1.bugPathWith2LopsKALE1926(driver);
-		//Bug KALE 1947
-		obj1.bugPath1KALE1947(driver);
-		//Bug KALE 1957
-		obj1.bugKALE1957(driver);
-		//Bug KALE 1846
-		obj1.bugKALE1846(driver);
-		//Bug KALE 1852
-		obj1.bugKALE1852(driver);
-		//Bug KALE 2219 and KALE 2246
-		obj1.bugKALE2219KALE2246(driver);
+		obj1.allBugsHiRCA(driver);
 		//Logout
 		obj.logout(driver);
 		afterTest(obj1);
--- QAAKALETests/KaleUSAHiRCALOPBugsChrome/src/test/java/ChromeTest.java
+++ QAAKALETests/KaleUSAHiRCALOPBugsChrome/src/test/java/ChromeTest.java
@@ -50,42 +50,25 @@ public class ChromeTest {
 		HiRCALOPBug obj1 = new HiRCALOPBug();
 		int login = obj.LoginUser(driver, username, password);
 		System.out.println("Title after login: "+driver.getTitle());
-		Thread.sleep(5000);
-		//Waits for the page to load
-	    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		//Switches to the iframe
 		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
-		Thread.sleep(5000);
 		if (login==1)
 		{
-          
-          while(true)
-		  {
-         	 Thread.sleep(1000);
-			  if (driver.findElement(By.cssSelector(".sticky.border-top-right.sticky-error")).isDisplayed())
-			  {
-				  WebElement ele =driver.findElement(By.cssSelector(".sticky.border-top-right.sticky-error"));
-				  ele.findElement(By.className("sticky-close")).click();
-				  break;
-			  }
-			  else break;
-		  }
-        }	
-		Thread.sleep(4000);
+
+			while(true)
+			{
+				Thread.sleep(1000);
+				if (driver.findElement(By.cssSelector(".sticky.border-top-right.sticky-error")).isDisplayed())
+				{
+					WebElement ele =driver.findElement(By.cssSelector(".sticky.border-top-right.sticky-error"));
+					ele.findElement(By.className("sticky-close")).click();
+					break;
+				}
+				else break;
+			}
+		}	
 		//Fill mandatory data
-		obj1.fillUpHiRCAEventInfo(driver,obj1.text);
-		//Bug KALE 1959
-		obj1.bugPath1KALE1959(driver);
-		//Bug KALE 1926
-		obj1.bugPathWith2LopsKALE1926(driver);
-		//Bug KALE 1947
-		obj1.bugPath1KALE1947(driver);
-		//Bug KALE 1957
-		obj1.bugKALE1957(driver);
-		//Bug KALE 1846
-		obj1.bugKALE1846(driver);
-		//Bug KALE 1852
-		obj1.bugKALE1852(driver);
+		obj1.allBugsHiRCA(driver);
 		//Logout
 		obj.logout(driver);
 		afterTest(obj1);
