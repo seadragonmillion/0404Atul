71bbd402d3e78cfe9cec5ea30cd2c7dfa9a02b1c
ritica <rramakrishnan@errorfree.com>
2019-07-17 15:38:19 -0700

    addded create sri test cases and qaa 755 to check for new text in case searches redesign

---
+++ QAAKALETests/CreateSRICases/src/test/java/ChromeTest.java
@@ -0,0 +1,67 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import kaleTestSoftware.CreateCaseSRI;
+import kaleTestSoftware.Login;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class ChromeTest {
+
+	private WebDriver driver;
+	private String username ="jenkinsvm";
+	private String password = "S2FsZWplbmtpbnNAMTIz";
+	private String chrome_path = "C:\\Users\\rramakrishnan\\DriversForSelenium\\chromedriver.exe";
+	private String url = System.getProperty("qaurl");
+
+	@Before
+	public void beforeTest() throws MalformedURLException{
+
+		System.out.println("Case Upload for SRI in Chrome");
+		System.setProperty("webdriver.chrome.driver",chrome_path);
+		driver = new ChromeDriver();
+		//Browser is maximized
+		driver.manage().window().maximize();
+		//Browser navigates to the KALE url
+		driver.navigate().to(url);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void test() throws Exception {
+		Login obj = new Login ();
+		CreateCaseSRI obj1 = new CreateCaseSRI ();
+		int login = obj.LoginUser(driver, username, password);
+		System.out.println("Title after login: "+driver.getTitle());
+		//Switches to the iframe
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
+		try{
+			if (login==1)
+			{
+				WebDriverWait wait2 = new WebDriverWait(driver,20);
+				wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-close"))).click();
+			}
+		}catch (NoSuchElementException e){
+			throw e;
+		}
+		//Create SRI cases
+		obj1.createCaseSRI(driver, obj1.keywordSRI, obj1.key1SRI, obj1.key2SRI, obj1.key3SRI, obj1.titleSRI);
+		//Logout
+		obj.logout(driver);
+		afterTest(obj1);		
+	}
+
+	public void afterTest(CreateCaseSRI obj1) throws Exception{
+		driver.quit();
+		obj1.softAssert();
+	}
+
+}
---
+++ QAAKALETests/CreateSRICases/src/test/java/FirefoxTest.java
@@ -0,0 +1,83 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import kaleTestSoftware.CreateCaseSRI;
+import kaleTestSoftware.Login;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class FirefoxTest {
+
+	private WebDriver driver;
+	private String username ="jenkinsvm";
+	private String password = "S2FsZWplbmtpbnNAMTIz";
+	private String gecko_path = "C:\\Users\\rramakrishnan\\DriversForSelenium\\geckodriver.exe";
+	private String url = System.getProperty("qaurl");
+
+	@Before
+	public void beforeTest() throws MalformedURLException{
+
+		System.out.println("Case Upload for SRI in Firefox");
+		System.setProperty("webdriver.gecko.driver",gecko_path);
+		ProfilesIni ffProfiles = new ProfilesIni();
+		FirefoxProfile profile = ffProfiles.getProfile("customFirefox");
+		FirefoxOptions options = new FirefoxOptions();
+		options.setCapability(FirefoxDriver.PROFILE, profile);
+		driver = new FirefoxDriver(options);
+		Dimension initialSize= driver.manage().window().getSize();
+		System.out.println(initialSize);
+		int height=initialSize.getHeight();
+		if(height<1900)
+		{
+			//Browser is maximized
+			driver.manage().window().maximize(); 
+		}
+		Dimension finalSize=driver.manage().window().getSize();
+		System.out.println(finalSize);
+		//Browser navigates to the KALE url
+		driver.navigate().to(url);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void test() throws Exception {
+		Login obj = new Login ();
+		CreateCaseSRI obj1 = new CreateCaseSRI ();
+		int login = obj.LoginUser(driver, username, password);
+		System.out.println("Title after login: "+driver.getTitle());
+		//Switches to the iframe
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='pii-iframe-main']")));
+		try{
+			if (login==1)
+			{
+				WebDriverWait wait2 = new WebDriverWait(driver,20);
+				wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("sticky-close"))).click();
+			}
+		}catch (NoSuchElementException e){
+			throw e;
+		}
+		//Create SRI cases
+		obj1.createCaseSRI(driver, obj1.keywordSRI, obj1.key1SRI, obj1.key2SRI, obj1.key3SRI, obj1.titleSRI);
+		//Logout
+		obj.logout(driver);
+		afterTest(obj1);		
+	}
+
+	public void afterTest(CreateCaseSRI obj1) throws Exception{
+		driver.quit();
+		obj1.softAssert();
+	}
+
+}
